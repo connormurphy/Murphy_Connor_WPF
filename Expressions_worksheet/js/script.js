7d8dff5ac06d7ff450a507410859c242db8da761
connormurphy <cmurphy4022@gmail.com>
2013-06-10 16:43:50 -0400

    Added Expression Worksheet and dog years problem.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,28 @@
+/*Connor Murphy
+June 10, 2013
+Expression Worksheet
+*/
+
+
+
+/* Dog Years */
+
+
+var sparkysAge = 7; //This is human age of Sparky
+var dogYears = 7 + sparkysAge;  // This shows how old Sparky is in "dog years"
+
+
+
+console.log(dogYears);
+
+
+
+//Slice of Pie Part 1
+
+var numSlices = 8;
+var numPeople = 16;
+var numPizzas = 4;
+
+
+
+
