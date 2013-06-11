1e32b05f557fe73c67e8bfd46cf286bb51414203
connormurphy <cmurphy4022@gmail.com>
2013-06-11 16:04:19 -0400

    Added ounces of water in gallon and cup.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,7 +7,17 @@ Expressions Wacky
 
 // How many boxes of jello would it take to fill your entire pool?
 
+//  There are 128oz in a a gallon
+// 8 oz of water in a cup
+// Each box of jello takes 2.5 cups of water.
+
+
+
+
 var gallons = prompt("How many gallons of water does your pool hold?");
-var feetAround = prompt("How many feet around is your pool?");
-var deep = prompt("How deep is your pool?");
+var inCup = prompt("How many ounces are in a cup?");
+var inGallon = prompt("How many ounces in a gallon?");
+
+var gallon = 128;
+var getOunces = gallon * gallons;
 
