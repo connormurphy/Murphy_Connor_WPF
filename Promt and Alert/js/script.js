301229a16083a52c136723bb9fd3832ce12143ab
connormurphy <cmurphy4022@gmail.com>
2013-06-11 12:56:10 -0400

    2 promts for area of rectangle.

--- Promt and Alert/js/script.js
+++ Promt and Alert/js/script.js
@@ -3,10 +3,8 @@ June 11, 2013
 Promt and Alert Exercise
 */
 
-var userInput = promt("Enter your year of birth:");
-
-console.log(userInput);
-
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
 
 
 
