b209da1ddafad2e4a360656c891d1ab91df0ca67
connormurphy <cmurphy4022@gmail.com>
2013-06-11 14:50:40 -0400

    Added parseInt to tell the code they are integers and to add them.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,14 +7,12 @@ Expressions Personal
 
 
 // How much chicken to buy for a family dinner
-var how_many_people = prompt("Enter the number of people in your family");
-var numPieces = prompt("Enter the number of pieces each person will eat:");
+var how_many_people = parseInt(prompt("Enter the number of people in your family:"));
+var how_many_guests = parseInt(prompt("Enter the number of guests your having over:"));
+var numPieces = parseInt(prompt("Enter the number of pieces each person will eat:"));
 
-var num_of_chickens = how_many_people * numPieces;
+var people = how_many_people + how_many_guests;
 
+var chicken = people * numPieces;
 
-alert("You will need to buy " + num_of_chickens + " pieces of chicken for your family dinner");
-
-
-
-
+alert("You will need to buy " + chicken + " pieces of chicken for your family dinner")
