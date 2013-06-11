afa7e10ea3aa6f5d96ab8e16a00761b441f34211
connormurphy <cmurphy4022@gmail.com>
2013-06-11 16:15:26 -0400

    Added all the comment lines to explain what each line of code does.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -19,14 +19,14 @@ var inCup = prompt("How many ounces are in a cup?");  // Enter however many ounc
 var inGallon = prompt("How many ounces in a gallon?"); // Enter however many ounces are in a gallon.
 
 var gallon = 128; // 128oz of water is in a typical gallon
-var getOunces = gallon * gallons;
+var getOunces = gallon * gallons;  //  This gets the number of ounces of water from the amount your pool can hold.
 
-var num_of_gallons = 20;
+var num_of_gallons = 20;  // This is divided by the number of ounces to get you the number of boxes of jello you will need.
 
-var getBoxes = getOunces / num_of_gallons;
+var getBoxes = getOunces / num_of_gallons;  
+// This calculates how many boxes of jello you will need depending on how much water your pool can hold.
 
-console.log(getBoxes);
 
 
-alert("To fill up your entire pool of jello, it would take " + getBoxes + " boxes of jello.");
+alert("To fill up your entire pool of jello, it would take " + getBoxes + " boxes of jello.");  //  Final printout of how many boxes you will need to fill your entire pool with jello based on how many gallons of water it can hold.
 
