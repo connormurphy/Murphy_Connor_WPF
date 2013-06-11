61a41ab3e479fe55cc63134cf091f02ec796f68b
connormurphy <cmurphy4022@gmail.com>
2013-06-11 15:00:28 -0400

    Created a calculation to find out how much coffee to order at my job.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,3 +5,13 @@ Expressions Industry
 
 
 
+// I work at a grocery store as an Assistant Grocery Manager and often have to write orders.  This problem will calculate how to order a specific item and how much you need of it.
+
+// Going to use Maxwell House Coffee for the example.
+
+//How many cases to order to last you for the next 3 days.
+
+var cases_in_back = prompt("Enter the number of cases you have in your backroom already:")
+var cases_sold_per_day = prompt("Enter the number of cases you sell per day:")
+var anticipated = prompt("Enter the number of cases you intend to sell in the next 3 days:")
+
