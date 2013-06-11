b659360665edeb9c89632a76eef282c5077749f5
connormurphy <cmurphy4022@gmail.com>
2013-06-11 15:57:01 -0400

    Added 3 question prompts for jello pool

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,5 +5,9 @@ Expressions Wacky
 
 
 
-// How to fill up your entire pool with jello
+// How many boxes of jello would it take to fill your entire pool?
+
+var gallons = prompt("How many gallons of water does your pool hold?");
+var feetAround = prompt("How many feet around is your pool?");
+var deep = prompt("How deep is your pool?");
 
