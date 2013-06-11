ba6be56e626d54f02b7eb02723fa833d65d902a9
connormurphy <cmurphy4022@gmail.com>
2013-06-11 15:09:11 -0400

    Added comments to each line explaining what they are and do.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -11,10 +11,10 @@ Expressions Industry
 
 //How many cases to order to last you for the next 3 days.
 
-var cases_in_back = prompt("Enter the number of cases you have in your backroom already:")
-var cases_sold_per_day = prompt("Enter the number of cases you sell per day:")
-var anticipated = prompt("Enter the number of cases you intend to sell in the next 3 days:")
+var cases_in_back = prompt("Enter the number of cases you have in your backroom already:")  // This tells you how much you already have in stock.
+var cases_sold_per_day = prompt("Enter the number of cases you sell per day:") // What you usually sell per day.
+var anticipated = prompt("Enter the number of cases you intend to sell in the next 3 days:") // How much you anticipate to sell in the next 3 days.
 
-var what_to_order = anticipated - cases_in_back;
+var what_to_order = anticipated - cases_in_back;  // This takes what you anticipate to sell, but also subtracts what you have in your backroom already so you dont order too much and create overstock.
 
-alert("You will need to order " + what_to_order +" cases of coffee to last you the next 3 days");
+alert("You will need to order " + what_to_order +" cases of coffee to last you the next 3 days"); // Final output on how many cases to order.
