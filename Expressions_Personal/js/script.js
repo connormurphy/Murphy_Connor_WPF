4782772700a21cc23517fedf19b16ec9324e112f
connormurphy <cmurphy4022@gmail.com>
2013-06-11 13:43:58 -0400

    Added a problem with 2 alerts

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,20 @@
+/*Connor Murphy
+June 11, 2013
+Expressions Personal
+*/
+
+
+
+
+// How much chicken to buy for a family dinner
+var how_many_people = prompt("Enter the number of people in your family");
+var numPieces = prompt("Enter the number of pieces each person will eat:");
+
+var num_of_chickens = how_many_people * numPieces;
+
+
+alert("You will need to buy " + num_of_chickens + " pieces of chicken for your family dinner");
+
+
+
+
