bcbb4ac78cc934db6b89a46bc1ba563fc9e5d344
connormurphy <cmurphy4022@gmail.com>
2013-06-17 17:37:53 -0400

    Adding folder for conditinals exercises

---
+++ Conditionals/js/script.js
@@ -0,0 +1,14 @@
+/*Connor Murphy
+June 11, 2013
+Conditionals Exercise
+*/
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+
+var area = width * height;
+console.log(area);
+
+
+alert("The area of your rectangle is " + area);
+
