3b2892f4d8b038ac8ad567d04ea997b0e5b52723
connormurphy <cmurphy4022@gmail.com>
2013-06-18 19:55:14 -0400

    Added stufff your face problem to worksheet

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -3,14 +3,14 @@ June 18, 2013
 Conditionals Worksheet
 */
 
-var budget = 100;
-var iphonePrice = 199.99;
-var wonLottery = true;
+// Stuff your face I
 
-//if the price less then budget AND our paycheck is over 300.
-if (iphonePrice < budget || wonLottery){ 
-	console.log("We can buy the phone!") ;
-	
-}else{ 
-	console.log ("No phone for you!!");
+var weight = 252;
+
+if(weight >= 250){
+	console.log("The competitor qualifies for the heavyweight division.");
+
+
+}else{
+	console.log("The competitor needs to gain some weight");
 }
