1787b7d96f608e614eea2ea8a5d07ef2adc9e706
connormurphy <cmurphy4022@gmail.com>
2013-06-18 19:47:11 -0400

    Adding Conditionals worksheet

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,16 @@
+/*Connor Murphy
+June 18, 2013
+Conditionals Worksheet
+*/
+
+var budget = 100;
+var iphonePrice = 199.99;
+var wonLottery = true;
+
+//if the price less then budget AND our paycheck is over 300.
+if (iphonePrice < budget || wonLottery){ 
+	console.log("We can buy the phone!") ;
+	
+}else{ 
+	console.log ("No phone for you!!");
+}
