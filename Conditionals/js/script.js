be6cea1b78c4577e6c9274dc363e75d2882a311b
connormurphy <cmurphy4022@gmail.com>
2013-06-18 19:40:38 -0400

    Using or operator

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -3,12 +3,12 @@ June 17, 2013
 Conditionals Exercise
 */
 
-var budget = 300;
+var budget = 100;
 var iphonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price less then budget AND our paycheck is over 300.
-if (iphonePrice < budget && paycheck > 300){ 
+if (iphonePrice < budget || wonLottery){ 
 	console.log("We can buy the phone!") ;
 	
 }else{ 
