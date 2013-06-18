126b93c334e35d42c5f4b93f0200b7aaa1595d31
connormurphy <cmurphy4022@gmail.com>
2013-06-18 18:09:33 -0400

    Added <=

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -3,14 +3,17 @@ June 17, 2013
 Conditionals Exercise
 */
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
 var sneakerLift = 2;
 //if the kid is over 48" in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
 	//code performed if condition is true.
 	console.log("You can ride the coaster!"); 
 }
 
+if(kidHeight <= minHeight){
+}
 
+console.log("Sorry, you've got some growing to do first!");
 
