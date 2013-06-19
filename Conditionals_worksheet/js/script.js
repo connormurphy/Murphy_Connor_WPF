b0e3b213eb719a26631c60577bfb1dd891aaecd8
connormurphy <cmurphy4022@gmail.com>
2013-06-18 20:23:30 -0400

    Added Last chance for gas problem to worksheet

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -14,3 +14,25 @@ if(weight >= 250){
 }else{
 	console.log("The competitor needs to gain some weight");
 }
+
+
+
+// Last chance for gas
+
+
+// can go 300miles if mpg is 25 and gas tank holds 12 gallons.
+
+var mpg = 25; // miles per gallon
+var gauge = 50;  // % of gas left
+var gasTank = 12;  // gallons
+
+
+var gasLeft = gasTank * gauge/100;
+var milesLeft = gasLeft * mpg;
+
+if(milesLeft < 200) {
+	console.log("You only have " + gasLeft + " gallons of gas left in your tank, better get gas now while you can!");
+	
+}else{ 
+	console.log("Yes you can make it without getting gas!");
+}
