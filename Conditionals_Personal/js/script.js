e7f1670692964b1df5eedf7e84238f6c60287c3e
connormurphy <cmurphy4022@gmail.com>
2013-06-19 14:21:59 -0400

    Added a message if you need a parent, and if you need to gain some weight.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -17,16 +17,19 @@ var weight = prompt ("How much do you weigh?");
 var height = prompt ("How tall are you in feet?");
 
 
-
 var qualify = howOld > requiredAge && weight > requiredWeight && height > requiredHeight;
 var noQualify = howOld < requiredAge && weight < requiredWeight && height < requiredHeight;
-var needParent = withParent
+
 
 if(noQualify == true){
 	console.log("You don't meet the qualifications to donate blood, sorry!");
 	
 }else if(howOld < 16){
 	console.log("You can donate blood, but only if you have a parent present.");
+}
+
+else if(weight < requiredWeight){
+	console.log("You need to gain some weight before your able to donate blood!");
 }
 
 
