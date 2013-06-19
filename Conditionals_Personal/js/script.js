b7cdf1b6e4fa57d7bc176a9dcaa3b72a307f0ef3
connormurphy <cmurphy4022@gmail.com>
2013-06-19 13:35:01 -0400

    Created a calculation to figure out if I can donate blood based on height, weight, and age

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,3 +3,11 @@ June 19, 2013
 Conditionals Personal
 */
 
+// I want to donate blood to United Blood Services.  This will calculate if I am able to do so based on a few variables.
+
+var howOld = prompt ("How old are you?")
+var weight = prompt ("How much do you weigh?")
+var height = prompt ("How tall are you?)
+
+
+if(){
