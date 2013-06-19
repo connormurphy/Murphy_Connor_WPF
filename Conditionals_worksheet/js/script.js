ce585581ccc9aeaf9c50b336d5d75db367699836
connormurphy <cmurphy4022@gmail.com>
2013-06-18 20:52:07 -0400

    Added grade letter calculator to worksheet

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -36,3 +36,36 @@ if(milesLeft < 200) {
 }else{ 
 	console.log("Yes you can make it without getting gas!");
 }
+
+
+
+
+// Grade letter calculator
+
+var A = 95 - 100;
+var B = 85 - 94;
+var C = 75-84;
+var D = 70 - 74;
+var F = 0 - 69;
+
+var studentGrade = 86;
+
+if(studentGrade < 100 && studentGrade > 95){
+	console.log("You have a " + studentGrade + "% which means you earned a A in the class!");
+}
+
+else if (studentGrade > 85 && studentGrade < 94){
+	console.log("You have a " + studentGrade + "% which means you earned a B in the class!");
+}
+
+else if (studentGrade > 75 && studentGrade < 84){
+	console.log("You have a " + studentGrade + "% which means you earned a C in the class!");
+}
+
+else if (studentGrade > 70 && studentGrade < 74){
+	console.log("You have a " + studentGrade + "% which means you earned a D in the class!");
+}
+
+else if (studentGrade > 0 && studentGrade < 69){
+	console.log("You have a " + studentGrade + "% which means you earned a F in the class!");
+}
