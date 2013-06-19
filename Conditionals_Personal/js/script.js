a252d376ad642e6c779603d01905482052dc5511
connormurphy <cmurphy4022@gmail.com>
2013-06-19 13:41:57 -0400

    Came up with variables, need to fix some things still

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,9 +5,15 @@ Conditionals Personal
 
 // I want to donate blood to United Blood Services.  This will calculate if I am able to do so based on a few variables.
 
-var howOld = prompt ("How old are you?")
-var weight = prompt ("How much do you weigh?")
-var height = prompt ("How tall are you?)
+var howOld = prompt ("How old are you?");
+var weight = prompt ("How much do you weigh?");
+var height = prompt ("How tall are you in feet?");
 
+var qualify = howOld >= 16 && weight >= 100 && height >= 5;
+var noQualify = howOld <= 16 && weight <= 100 && height <= 5;
 
-if(){
+if(qualify = true){
+	console.log("You meet the qualifications to donate blood!");
+}else{
+	console.log("You dont meet the qualifications to donate blood, please try at a later time.");
+}
