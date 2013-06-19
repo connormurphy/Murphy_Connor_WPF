8939216db3111565e5d4b35a5c4672c30a2a10bb
connormurphy <cmurphy4022@gmail.com>
2013-06-19 14:17:55 -0400

    Fixed code, now have an if, else if, and else

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,17 +5,36 @@ Conditionals Personal
 
 // I want to donate blood to United Blood Services.  This will calculate if I am able to do so based on a few variables.
 
+
+var requiredAge = 16;
+var requiredWeight = 110;
+var requiredHeight = 5;
+
+
+
 var howOld = prompt ("How old are you?");
 var weight = prompt ("How much do you weigh?");
 var height = prompt ("How tall are you in feet?");
 
-var qualify = howOld >= 16 && weight >= 100 && height >= 5;
-var noQualify = howOld <= 16 && weight <= 100 && height <= 5;
 
-if(qualify = true){
+
+var qualify = howOld > requiredAge && weight > requiredWeight && height > requiredHeight;
+var noQualify = howOld < requiredAge && weight < requiredWeight && height < requiredHeight;
+var needParent = withParent
+
+if(noQualify == true){
+	console.log("You don't meet the qualifications to donate blood, sorry!");
+	
+}else if(howOld < 16){
+	console.log("You can donate blood, but only if you have a parent present.");
+}
+
+
+else{
 	console.log("You meet the qualifications to donate blood!");
 }
 
-if(noQualify = true){
-	console.log("You dont meet the qualifications to donate blood, sorry!");
-}
+
+
+
+
