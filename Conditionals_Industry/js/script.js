684148a593c2fd20e4d6bb9cf9c2d80048f192e2
connormurphy <cmurphy4022@gmail.com>
2013-06-20 14:37:30 -0400

    Trying to add a 10% discount for anything after 5 pages.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -8,16 +8,25 @@ Conditionals Industry
 var pages = prompt ("How many pages do you need designed and developed?");
 var budget = prompt ("What is your budget for this project?");
 
-//var moneyResponsive = 120;
-//var moneyCms = 300;
 var totalPages = pages * 90;
+var discount = totalPages / .10;
 
 
 (totalPages <= budget) ? console.log("You entered a total of " + pages + " pages, and said your budget is $" + budget + ".  You have enough money!") : console.log("You entered a total of " + pages + " pages, and said your budget is $" + budget + ".  You need more money, sorry!");
 
 
 
-//var responsiveCms = responsive == "yes" && cms == "yes";
+/*if(pages > 5){
+	console.log("You entered a total of " + pages + " pages, and said you budget is $" + budget + ".  You have enough money, and I've even given you a 10% discount since you need more then 5 pages!");
+}
+
+else{
+	console.log("You entered a total of " + pages + " pages, and said your budget is $" + budget + ".  You need more money, sorry!");
+}
+	*/
+
+
+
 
 
 
