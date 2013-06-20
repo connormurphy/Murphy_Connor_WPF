e0eb31e38a497d8f10a31572e52d3004e3a3f59b
connormurphy <cmurphy4022@gmail.com>
2013-06-20 15:13:00 -0400

    Coding a BAC calculator

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,3 +3,10 @@ June 19, 2013
 Conditional Wacky
 */
 
+// Im going to create a Blood alcohol Content calculator that helps a person see the BAC %.  This is all based on a 12oz beer.
+
+
+var beers = prompt ("How many beers have you had?");
+var gender = prompt ("Male or female?");
+var weight = prompt ("How much do you weigh?");
+var hours = prompt ("How many hours has it been since you stopped drinking?");
