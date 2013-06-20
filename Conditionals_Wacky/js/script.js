137937ad2911e67766a183345e96c7689ba130fb
connormurphy <cmurphy4022@gmail.com>
2013-06-20 15:28:32 -0400

    Added male and female BAC level based on user inputted information.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,11 +8,20 @@ Conditional Wacky
 
 var beers = prompt ("How many beers have you had?");
 var weight = prompt ("How much do you weigh?");
+var gender = prompt ("Male or female?");
 var hours = prompt ("How many hours has it been since you stopped drinking?");
 
-var ozBeers = beers * 12;
+var ozBeers = beers * 12 * 0.06;
 
 
 var bacMale = (ozBeers * 5.14/weight * 0.73) - .015 * hours;
 
 var bacFemale = (ozBeers * 5.14/weight * 0.66) - .015 * hours;
+
+if(gender = "male"){
+	console.log("Your BAC level is " + bacMale + "%");
+}
+
+else if(gender = "female"){
+	console.log("Your BAC level is " + bacFemale + "%");
+}
