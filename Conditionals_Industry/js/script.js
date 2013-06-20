733b125ef5f68aee9d2f52c8b7d60c906e3e3001
connormurphy <cmurphy4022@gmail.com>
2013-06-20 13:44:52 -0400

    Added prompts to collect user information

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,3 +5,8 @@ Conditionals Industry
 
 // Conditionals Industry.  Im creating a script to be used by a client that will tell me what kind of website they need designed, and how much money it will cost them.  The user will be able to tell me if they want it responsive, if they need a CMS, how many pages they need, and what their budget is.  The print outs will tell them if they have enough money, or if they need more.  It will also tell them what they can get for the budget they entered.
 
+var responsive = prompt ("Do you need your website to be responsive? (Yes or no)");
+var cms = prompt ("Do you need a content management system? (Yes or no)");
+var pages = prompt ("How many pages do you need designed and developed?");
+var budget = prompt ("What is your budget for this project?");
+
