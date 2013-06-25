41b8e8ccad9b3ed045ef2ddea94181c31c575cb5
connormurphy <cmurphy4022@gmail.com>
2013-06-25 14:07:22 -0400

    Added dogYears function

--- Functions Practice/js/script.js
+++ Functions Practice/js/script.js
@@ -3,12 +3,11 @@ June 17, 2013
 Functions Practice
 */
 
-var width = 5;
-
-
-calcArea(30, 20);
-
-function calcArea (w, h){ //w=30, h=20
-	var area = w * h;
-	console.log(area);
+function dogYears(age){
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + " years old.")
 }
+
+
+dogYears(4);
+dogYears(5);
