84c107b8439aec2139f8250430da9e892a2984b7
connormurphy <cmurphy4022@gmail.com>
2013-06-27 15:21:47 -0400

    Added an alert message to inform average life expectancy as of 2013

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,6 +5,8 @@ Functions Wacky
 
 // This calculator tells you how many months you will work throughout your life, and how much time you have left after you retire before you die.  This is all based on an average life expectancy of 79 years old.
 
+var fact = alert("The average life expectancy in the US as of 2013 is about 79 years old.");
+
 var age = prompt("How old are you now?");
 var startWork = prompt("What age did you start working?");
 var retire = prompt("What age do you plan to retire?");
