b4935122135f5bff6f890ffcaddc87a8096eaa88
connormurphy <cmurphy4022@gmail.com>
2013-06-27 16:42:05 -0400

    Added comment lines to first function

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,23 +5,23 @@ Functions Wacky
 
 // This calculator tells you how many months you will work throughout your life, and how much time you have left after you retire before you die.  This is all based on an average life expectancy of 79 years old.
 
-var fact = alert("The average life expectancy in the US as of 2013 is about 79 years old.");
+var fact = alert("The average life expectancy in the US as of 2013 is about 79 years old."); // This is to tell the user this script is based on a person living until they are 79.
 
-var startWork = prompt("What age did you start working?");
+var startWork = prompt("What age did you start working?"); // prompt questions to gather user information
 var age = prompt("How old are you now?");
 var retire = prompt("What age do you plan to retire?");
 
 
-var totalSoFar = yearsSoFar(startWork, age, retire);
+var totalSoFar = yearsSoFar(startWork, age, retire); // calls the function
 
-function yearsSoFar (startWork, age, retire){
-	var soFar = age - startWork;
-	return soFar;
+function yearsSoFar (startWork, age, retire){ // function for years worked so far
+	var soFar = age - startWork; // calculation for how long user has worked so far
+	return soFar; // returns to totalSoFar above.
 }
 
 
 
-var totalTimeWorked = startToRetire(startWork, age, retire);
+var totalTimeWorked = startToRetire(startWork, age, retire); // calls the function
 
 function startToRetire (startWork, age, retire){
 	var startToFinish = retire - startWork;
