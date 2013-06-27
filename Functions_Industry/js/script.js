2603e536c5a9692ea786812e5c2dcedaa7f15f65
connormurphy <cmurphy4022@gmail.com>
2013-06-27 14:07:47 -0400

    Added option for result to be displayed in inches or feet

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -10,6 +10,8 @@ var length = prompt("What is the length of the room? (inches)"); // length of ro
 var width = prompt("What is the width of the room? (inches)"); // width of room
 var height = prompt("What is the height of the room? (inches)"); // height of room
 
+var question = prompt("Do you want the result displayed in inches or feet?");
+
 var getVolume = function(length, width, height){ // function
 	var volume = length * width * height; // calculation to get volume
 	return volume; // this returns the information to the printout
@@ -18,4 +20,10 @@ var getVolume = function(length, width, height){ // function
 
 var useFunction = getVolume(length, width, height); // this is used to call the function and printout what the actual calculation is.
 
-console.log("The volume of this room is " + useFunction + " cubic inches.  The length was " + length + "
+
+if(question = "inches"){
+console.log("The volume of this room is " + useFunction + " cubic inches.  The length was " + length + " inches, the width was " + width + " inches, and the height was " + height + " inches.");
+// this prints out the calculation for the volume of the room.
+}else{
+	console.log("The volume of this room is " + useFunction + " cubic inches.  The length was " + length + " inches, the width was " + width + " inches, and the height was " + height + " inches.");
+}
