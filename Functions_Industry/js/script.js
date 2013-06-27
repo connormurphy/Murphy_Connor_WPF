eabfcb43986a783492112925cb48ddbd681f20d0
connormurphy <cmurphy4022@gmail.com>
2013-06-27 13:50:27 -0400

    added function to get volume of the room based on user information

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -10,3 +10,6 @@ var length = prompt("What is the length of the room?"); // length of room
 var width = prompt("What is the width of the room?"); // width of room
 var height = prompt("What is the height of the room?"); // height of room
 
+var getVolume = function(length, width, height){
+	var volume = length * width * height;
+
