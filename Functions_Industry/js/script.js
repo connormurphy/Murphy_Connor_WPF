032ae4a8f110e9b7ffa7f59f71f234a7893f7f12
connormurphy <cmurphy4022@gmail.com>
2013-06-27 13:48:30 -0400

    Added comment lines for 3 prompts.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,7 +6,7 @@ Functions Industry
 	// How to find the volume of our backroom at work.
 	
 
-var length = prompt("What is the length of the room?");
-var width = prompt("What is the width of the room?");
-var height = prompt("What is the height of the room?");
+var length = prompt("What is the length of the room?"); // length of room
+var width = prompt("What is the width of the room?"); // width of room
+var height = prompt("What is the height of the room?"); // height of room
 
