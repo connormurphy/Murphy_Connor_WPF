649298f55c4ef382759a82e0d2de91fbcbbf6004
connormurphy <cmurphy4022@gmail.com>
2013-06-27 13:47:40 -0400

    Added 3 user prompts to gather information about the room

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,5 +6,7 @@ Functions Industry
 	// How to find the volume of our backroom at work.
 	
 
-
+var length = prompt("What is the length of the room?");
+var width = prompt("What is the width of the room?");
+var height = prompt("What is the height of the room?");
 
