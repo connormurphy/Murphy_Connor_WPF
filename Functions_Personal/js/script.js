337b8ba58c936cde0de9f1b33f8ca2d9680fc31a
connormurphy <cmurphy4022@gmail.com>
2013-06-27 02:18:46 -0400

    Added variables for 1 week, expected amount of water, and added function

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,5 +3,12 @@ June 26 2013
 Functions Personal
 */
 
-// This will calculate if you drink enough water 
+// This will calculate if you drink enough water each week.  It's recommended you drink 8, 8oz glasses each day.
 
+var glasses = prompt("How many glasses of water do you drink daily?")
+var weeks = 1;
+var expected = 8;
+
+
+var calcWater = function(weeks, glasses){
+	var total = weeks * glasses;
