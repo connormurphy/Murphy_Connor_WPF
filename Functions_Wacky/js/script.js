f830fd34cddba5dfe4e8ddf5645d23d88b22f5d3
connormurphy <cmurphy4022@gmail.com>
2013-06-27 15:52:35 -0400

    Have the main calculator finished, trying to work out some problems.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -11,16 +11,28 @@ var startWork = prompt("What age did you start working?");
 var age = prompt("How old are you now?");
 var retire = prompt("What age do you plan to retire?");
 
+var yearsToMonths1 = yearsWorked * 12;
+var yearsToMonths2 = yearsLeft * 12;
+
+var totalTimeWorked = retire - startWork * 12;
+var monthsLeft = 79 - retire * 12;
+
+
+var totalYearsWorked = calcYearsWorked (age, startWork, retire);
+var totalYearsLeft = calcYearsLeft (age, startWork, retire);
+
+
+
 var calcYearsWorked = function (age, startWork, retire){
 	var yearsWorked = age - startWork;
 	return yearsWorked;
 }
 
 var calcYearsLeft = function (age, startWork, retire){
-	var yearsLeft = 79 - monthsWorked;
+	var yearsLeft = 79 - yearsWorked;
 	return yearsLeft;
 }
 
-var totalMonthsWorked = calcMonthsWorked (age, startWork, retire);
-var totalMonthsLeft = calcMonthsLeft (age, startWork, retire);
 
+
+console.log("So far, you've been working for " + totalYearsWorked + " years.  Since your planning to retire at age " + retire + ", you will work for a total of " + totalTimeWorked + " months in your life.  This leaves you with " + monthsLeft + " months to do whatever you want to do!");
