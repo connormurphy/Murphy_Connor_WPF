b16d6bf3d572cc5a135f7f560178152b93b4b839
connormurphy <cmurphy4022@gmail.com>
2013-06-27 01:06:12 -0400

    Adding functions worksheet

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,15 @@
+/*Connor Murphy
+June 26 2013
+Functions Worksheet
+*/
+
+var calcArea = function(width, height){
+	//code the function runs
+	var area = width * height;
+	return area;
+	
+}
+
+var a = calcArea(20,30);
+
+console.log(a); 
