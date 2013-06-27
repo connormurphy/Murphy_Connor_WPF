ff0f3b2116932f7649f1a12db3a9d6695c10080c
connormurphy <cmurphy4022@gmail.com>
2013-06-27 15:16:44 -0400

    Added the description for what I am going to make

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,3 +3,6 @@ June 27, 2013
 Functions Wacky
 */
 
+// This calculator tells you how many months you will work throughout your life, and how much time you have left after you retire before you die.  This is all based on an average life expectancy of 79 years old.
+
+
