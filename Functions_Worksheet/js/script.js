9bd36aabbfa3cb90a7a19d7acee4c842c480adbd
connormurphy <cmurphy4022@gmail.com>
2013-06-27 01:24:28 -0400

    Added printout message after calling the function

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -6,6 +6,13 @@ Functions Worksheet
 // Circumference of a circle
 
 
+
 function circumference (){
 	var radius = 6;
-	var get_circumference = 2 * 3.14 * radius;
+	var getCircum = 2 * 3.14 * radius;
+	console.log("The circumference of the circle is " + getCircum + "")
+	
+}
+circumference();
+
+
