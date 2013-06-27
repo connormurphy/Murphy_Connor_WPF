df1f94c087bed87f31a97b656dd6ac496dd72275
connormurphy <cmurphy4022@gmail.com>
2013-06-27 13:54:08 -0400

    Added function call, and how to printout the information

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -10,6 +10,10 @@ var length = prompt("What is the length of the room?"); // length of room
 var width = prompt("What is the width of the room?"); // width of room
 var height = prompt("What is the height of the room?"); // height of room
 
-var getVolume = function(length, width, height){
-	var volume = length * width * height;
+var getVolume = function(length, width, height){ // function
+	var volume = length * width * height; // calculation to get volume
+	return volume; // this returns the information to the printout
+	
+}
 
+var useFunction = getVolume(length, width, height); // this is used to call the function and printout what the actual calculation is.
