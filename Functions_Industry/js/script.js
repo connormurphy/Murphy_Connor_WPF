934fbfa2b31aaae7bbb89396c2daf52767477a2c
connormurphy <cmurphy4022@gmail.com>
2013-06-27 13:58:51 -0400

    Added labels to prompts to let users enter information as inches

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,9 +6,9 @@ Functions Industry
 	// How to find the volume of our backroom at work.
 	
 
-var length = prompt("What is the length of the room?"); // length of room
-var width = prompt("What is the width of the room?"); // width of room
-var height = prompt("What is the height of the room?"); // height of room
+var length = prompt("What is the length of the room? (inches)"); // length of room
+var width = prompt("What is the width of the room? (inches)"); // width of room
+var height = prompt("What is the height of the room? (inches)"); // height of room
 
 var getVolume = function(length, width, height){ // function
 	var volume = length * width * height; // calculation to get volume
@@ -17,3 +17,5 @@ var getVolume = function(length, width, height){ // function
 }
 
 var useFunction = getVolume(length, width, height); // this is used to call the function and printout what the actual calculation is.
+
+console.log("The volume of this room is " + useFunction + " cubic inches.  The length was " + length + "
