156eff7ec3bd31f3961603856567ed1310ee6cec
connormurphy <cmurphy4022@gmail.com>
2013-06-27 02:23:11 -0400

    Added if and else statement

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -12,3 +12,15 @@ var expected = 8;
 
 var calcWater = function(weeks, glasses){
 	var total = weeks * glasses;
+	return total;
+}
+
+var totalGlasses = calcWater(weeks, glasses);
+
+if(totalGlasses >= expected){
+	console.log("You are on the right track and drink enough water throughout the week to stay hydrated!");}
+
+else{
+	console.log("You are only drinking " + glasses + " glasses of water daily.  It's recommended you drink 8 to stay hydrated!");
+}
+	
