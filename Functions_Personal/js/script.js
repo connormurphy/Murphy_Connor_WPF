9d247b0ad9ca3bf4831b7325a97cb32073d6640f
connormurphy <cmurphy4022@gmail.com>
2013-06-27 02:27:39 -0400

    Added comment lines to explain what each line of code does

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,22 +5,22 @@ Functions Personal
 
 // This will calculate if you drink enough water each week.  It's recommended you drink 8, 8oz glasses each day.
 
-var glasses = prompt("How many glasses of water do you drink daily?")
-var weeks = 1;
-var expected = 8;
+var glasses = prompt("How many glasses of water do you drink daily?") // this asks the user how much water they drink per day
+var weeks = 1; // this is over the course of a week
+var expected = 8; // this is the expected amount of glasses of water per day.
 
 
-var calcWater = function(weeks, glasses){
-	var total = weeks * glasses;
+var calcWater = function(weeks, glasses){ // defining the function
+	var total = weeks * glasses; // getting calculation.
 	return total;
 }
 
-var totalGlasses = calcWater(weeks, glasses);
+var totalGlasses = calcWater(weeks, glasses); // named the function
 
 if(totalGlasses >= expected){
-	console.log("You are on the right track and drink enough water throughout the week to stay hydrated!");}
+	console.log("You are on the right track and drink enough water throughout the week to stay hydrated!");} // this tells user if they are drinking enough water
 
 else{
 	console.log("You are only drinking " + glasses + " glasses of water daily.  It's recommended you drink 8 to stay hydrated!");
-}
+} // if they arent drinking enough, this is the message they get
 	
