54c4a99bc143ad056ea6aefe4e826dd3ed6e07f5
connormurphy <cmurphy4022@gmail.com>
2013-06-27 14:54:13 -0400

    Conversion calculator wasn't working, back to original

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -10,11 +10,7 @@ var length = prompt("What is the length of the room? (inches)"); // length of ro
 var width = prompt("What is the width of the room? (inches)"); // width of room
 var height = prompt("What is the height of the room? (inches)"); // height of room
 
-var question = prompt("Do you want the result displayed in inches or feet?");
 
-var feetLength = length * 0.08333
-var feetWidth = width * 0.08333
-var feetHeight = height *  0.08333
 
 var getVolume = function(length, width, height){ // function
 	var volume = length * width * height; // calculation to get volume
@@ -25,12 +21,7 @@ var getVolume = function(length, width, height){ // function
 var useFunction = getVolume(length, width, height); // this is used to call the function and printout what the actual calculation is.
 
 
-/*if(question = "inches"){
 console.log("The volume of this room is " + useFunction + " cubic inches.  The length was " + length + " inches, the width was " + width + " inches, and the height was " + height + " inches.");
 // this prints out the calculation for the volume of the room.
-}else{
-	console.log("The volume of this room is " + useFunction + " cubic inches.  The length was " + feetLength + " feet, the width was " + feetWidth + " feet, and the height was " + feetHeight + " feet.");
-}
-*/
 
-(question = "inches") ? console.log("The volume of this room is " + useFunction + " cubic inches.  The length was " + length + " inches, the width was " + width + " inches, and the height was " + height + " inches.") : console.log("The volume of this room is " + useFunction + " cubic inches.  The length was " + feetLength + " feet, the width was " + feetWidth + " feet, and the height was " + feetHeight + " feet.");
+
